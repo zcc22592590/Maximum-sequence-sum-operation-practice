6f5a359708f62970fcd4d954616cbb2e5c360351
zcc22592590 <75067139+zcc22592590@users.noreply.github.com>
2021-03-07 17:50:03 +0800

    Add files via upload

---
+++ Maximum-sequence-sum-operation(ex2).c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	int n;
+	int low[j], up[j];
+	int sum = 0, max = 0;
+	int a, b;
+	int k;
+	
+	printf("enter a number: \n");
+	scanf("%d", &n);
+	
+	for(j=0; j<n; j++){
+		printf("enter two number:");
+		scanf("%d %d", &low[j], &up[j]);
+	}
+	
+	for(k=0; k<n; k++)
+	{
+		for(i=low[k]; i<=up[k]; i++)
+		{
+			a = i;
+			while(a!=1)
+			{
+				sum += a;
+				if(a%2==1)
+				{
+					a = 3*a + 1;
+				}
+				else
+				{
+					a = a / 2;
+				}
+			}	
+			
+			if(sum>=max){
+				max = sum + 1;
+				b = i;
+			}
+			sum = 0;
+		}
+		printf("%d %d\n", max, b);
+		max = 0;
+		b = 0;
+	}
+}
