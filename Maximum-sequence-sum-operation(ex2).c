cc4ef5acfb865f7fecbc22f4269abe39291d30cc
zcc22592590 <75067139+zcc22592590@users.noreply.github.com>
2021-03-09 17:37:03 +0800

    final version

--- Maximum-sequence-sum-operation(ex2).c
+++ Maximum-sequence-sum-operation(ex2).c
@@ -1,48 +1,41 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-	int i, j;
-	int n;
-	int low[5], up[5];
-	int sum = 0, max = 0;
-	int a, b;
-	int k;
-	
-	printf("enter a number: \n");
-	if(scanf("%d", &n)){};
-	
-	for(j=0; j<n; j++){
-		printf("enter two number:");
-		if(scanf("%d %d", &low[j], &up[j])){};
-	}
-	
-	for(k=0; k<n; k++)
-	{
-		for(i=low[k]; i<=up[k]; i++)
-		{
-			a = i;
-			while(a!=1)
+ 	int n,i,j,k,sum=0,max=0,a,b; 
+ 	scanf("%d",&n);
+ 	int LowerBound[n],UpperBound[n];
+ 	for(j=0;j<n;j++)
+ 	{
+  		scanf("%d %d",&LowerBound[j],&UpperBound[j]); 
+ 	}
+ 	for(k=0;k<n;k++)
+ 	{ 
+ 		for(i=LowerBound[k];i<=UpperBound[k];i++)
+ 		{
+  			a=i;
+  			while(a!=1)
+  			{
+   				sum+=a;
+   				if(a%2==1)
+   				{
+    				a=3*a+1;    
+   				}
+   				else
+   				{
+    				a=a/2;
+   				}   
+  			}		
+  			if(sum>=max)
 			{
-				sum += a;
-				if(a%2==1)
-				{
-					a = 3*a + 1;
-				}
-				else
-				{
-					a = a / 2;
-				}
-			}	
-			
-			if(sum>=max){
-				max = sum + 1;
-				b = i;
-			}
-			sum = 0;
-		}
-		printf("%d %d\n", max, b);
-		max = 0;
-		b = 0;
-	}
+   				max=sum+1;
+   				b=i;
+  			}	  
+  			sum=0;  
+ 		}
+ 		printf("%d %d\n",max,b);
+ 		max=0;
+ 		b=0;
+ 
+ 	}
+ 	return(0);
 }
